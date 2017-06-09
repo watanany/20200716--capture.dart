701f004f92e83fe71e83df4c881a18224273e710
Watanabe Shingo <s1170087@gmail.com>
2017-06-09 14:59:35 +0900

    modify: according to dartanalyzer

--- web/main.dart
+++ web/main.dart
@@ -1,15 +1,14 @@
-import 'dart:developer';
 import 'dart:html';
 
 void main() {
-  final capture = document.querySelector('#capture');
+  final ButtonElement capture = document.querySelector('#capture');
   capture.onClick.listen((event) {
-    final video = document.querySelector('#video');
-    final canvas = document.querySelector('#canvas');
-    final download = document.querySelector('#download');
+    final VideoElement video = document.querySelector('#video');
+    final CanvasElement canvas = document.querySelector('#canvas');
+    final AnchorElement download = document.querySelector('#download');
 
-    canvas.setAttribute('width', video.videoWidth);
-    canvas.setAttribute('height', video.videoHeight);
+    canvas.setAttribute('width', video.videoWidth.toString());
+    canvas.setAttribute('height', video.videoHeight.toString());
     canvas.context2D.drawImage(video, 0, 0);
 
     download.setAttribute('href', canvas.toDataUrl('image/png'));
